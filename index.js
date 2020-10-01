607d8c4e92b03c662cebb9697a586dccaa10bbe0
arifintahu <miftahul97@gmail.com>
2020-10-01 19:32:20 +0700

    deploy

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const loaders = require('./src/loaders');
 
 async function startServer() {
     const app = express();
+    const port = process.env.PORT || 3000;
 
     await loaders(app);
-    app.listen(process.env.PORT, (error) => {
+    app.listen(port, (error) => {
         if (error) {
             process.exit(1);
         }
-        console.log('Server is listening on port: ', process.env.PORT);
+        console.log('Server is listening on port: ', port);
   });
 }
 
