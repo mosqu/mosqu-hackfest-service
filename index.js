f6e25375d04cdb48d48912cba82817ddd75287b2
arifintahu <miftahul97@gmail.com>
2020-12-17 14:11:05 +0700

    add socket io

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-const WebSocket     = require('ws');
 const express       = require('express');
 const fileUpload    = require('express-fileupload');
 const { Client }    = require('whatsapp-web.js');
 const loaders       = require('./src/loaders');
 
-const wss           = new WebSocket.Server({ noServer: true });
-
 async function startServer() {
-    const app = express();
+    const app       = express();
     app.use(fileUpload());
-    
-    const port = process.env.PORT || 3000;
+    const port      = process.env.PORT || 3000;
 
-    const server    = await loaders(app);
+    const app2      = await loaders(app);
+    const server    = require("http").createServer(app2);
     const route     = server.listen(port, (error) => {
         if (error) {
             process.exit(1);
@@ -20,67 +17,57 @@ async function startServer() {
         console.log('Server is listening on port: ', port);
     });
 
-    route.on('upgrade', (request, socket, head) => {
-        const topic = request.url.slice(1);
-
-        if (!topic) {
-            socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
-            socket.destroy();
-            return;
-        }
-
-        wss.handleUpgrade(request, socket, head, (ws) => {
-            wss.emit(topic, ws, request);
-        });
+    const io        = require("socket.io")(server, {
+      cors: {
+        origin: '*',
+      }
     });
 
-    wss.on('blast', (ws, request) => {
-        ws.on('message', async (data) => {
+    io.on('connection', function (socket) {
+        console.log("Connected succesfully to the socket ...");        
+
+        socket.on('blast', function (content) {
+            console.log(content);
+
             const client        = new Client();
-            const content       = JSON.parse(data);
 
             client.on('qr', (qr) => {
                 console.log('QR RECEIVED', qr);
-                ws.send(JSON.stringify({
+                socket.emit('blast/response', {
                     action: 'qr',
                     msg: qr
-                }));
+                });
             });
              
             client.on('ready', () => {
                 console.log('Client is ready!');
-                ws.send(JSON.stringify({
+                socket.emit('blast/response', {
                     action: 'ready',
                     msg: 'ready'
-                }));
+                });
                 if (content.message && content.phone) {
                     client.sendMessage(`${content.phone}@c.us`, content.message)
                     .then(() => {
-                        ws.send(JSON.stringify({
+                        socket.emit('blast/response', {
                             action: 'done',
                             msg: 'success'
-                        }));
+                        });
                     })
                     .catch((error) => {
-                        ws.send(JSON.stringify({
+                        socket.emit('blast/response', {
                             action: 'done',
                             msg: 'error'
-                        }));
+                        });
                         console.log(error);
                     });
                 }
 
             });
-             
-            client.on('message', msg => {
-                if (msg.body == '!ping') {
-                    msg.reply('pong');
-                }
-            });
 
             client.initialize().catch((error) => {
                 console.log(error);
             });
+
         });
     });
 }
